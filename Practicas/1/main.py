833a7a9d16f1ce01e58fc0d49f12425d9f7540fd
Ricardo0919 <rickisierra03@gmail.com>
2025-03-31 17:48:30 -0600

    Update

--- Practicas/1/main.py
+++ Practicas/1/main.py
@@ -4,12 +4,15 @@ import threading
 import time
 
 drone = Tello()
-#Establece conexión de Wifi
+# Establece conexión de Wifi
 drone.connect()
-#Inicia el stream de video
+# Inicia el stream de video
 drone.streamon()
-#Revisar el estado de la bateria
+# Revisar el estado de la bateria
 print(drone.get_battery())
+
+# Iniciar la variable drone_flying
+drone_flying = False
 
 
 def clean_exit():
@@ -27,13 +30,42 @@ def video_stream():
         # Obteniendo el último frame del video
         frame = drone.get_frame_read().frame
 
-        #Mostrando la imagen
+        # Mostrando la imagen
         cv2.imshow("Stream de video", frame)
 
-        #Cerrar al presionar 'q'
-        if cv2.waitKey(10) & 0xFF == ord('q'):
+        # Moviendo el drone con el teclado
+        key = cv2.waitkey(10) & 0xFF
+
+        # Cerrar al presionar 'q'
+        if key == ord('q'):
             clean_exit()
             break
+
+        # Take off al presionar 't'
+        elif key == ord('t'):
+            if not drone_flying:
+                drone.takeoff()
+                drone_flying = True
+
+        # Aterrizar con 'l'
+        elif key == ord('l'):
+            if drone_flying:
+                drone.land()
+                drone_flying = False
+
+        # Mover hacia adelante con 'w'
+        elif key == ord('w'):
+            drone_fb_speed = 60
+
+        # Mover hacia atras con 's'
+        elif key == ord('s'):
+            drone_fb_speed = -60
+
+        else:
+            drone_fb_speed = 0
+
+        if drone_flying:
+            drone.send_rc_control(0, drone_fb_speed, 0, 0)
 
 
 def control():
